bcd9cf760dc57730df0d9090d41a784d0ec96e5a
Chuck D'Antonio <c_dantonio@acm.org>
2016-04-01 11:07:40 -0400

    Added simple installer for packages I use

---
+++ bin/packages.sh
@@ -0,0 +1,45 @@
+brew cask install 0xed
+brew cask install 1password
+brew cask install alfred
+brew cask install atext
+brew cask install atom
+brew cask install caffeine
+brew cask install dash
+brew cask install evernote
+brew cask install flux
+brew cask install flycut
+brew cask install github-desktop
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install intellij-idea
+brew cask install istumbler
+brew cask install java
+brew cask install postman
+brew cask install rubymine
+brew cask install screenhero
+brew cask install shiftit
+brew cask install skype
+brew cask install slack
+brew cask install spotify
+brew cask install textmate
+brew cask install vagrant
+brew cask install virtualbox
+
+brew install ack
+brew install autoconf
+brew install awscli
+brew install cf
+brew install direnv
+brew install git-flow
+brew install go
+brew install gradle
+brew install jenkins
+brew install maven
+brew install nexus
+brew install openssl
+brew install pkg-config
+brew install rbenv
+brew install ruby-build
+brew install springboot
+brew install wget
+brew install zsh-completions
