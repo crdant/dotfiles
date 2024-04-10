4d98c3f349c29f1a71eed1a530831398cfb89f5c
Chuck D'Antonio <chuck@replicated.com>
2024-04-10 09:14:14 -0400

    Assures user type and groups

--- users/crdant/crdant.nix
+++ users/crdant/crdant.nix
@@ -4,6 +4,8 @@
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
   users.users.crdant = {
+    isNormalUser = true;
+
     home = "/Users/crdant";
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
@@ -13,6 +15,13 @@
       "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBHjhrp9FP2DfpzUEtZ8e/h1lFCBZbAO4pOdD/toikmzV7Mdh0zTlqHUEWrRA6zeQd9LLk2P352LOt75YHCZ87QoAAAAEc3NoOg=="
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDX6f72jpsYAWdlQGLNlGeoz6AKsUl6FGDp48k/Z44QGqZdGymk3JUDO28BHBXJ05u6NiIad+x3hOYuKxglSjVjv8obqNnqAoJM8r2VLErlqwJNSSOzUijeA9V256Mq1ej7jTtYiNspI9TuyE9TOFsiS81tSIqgOGMJAe4Puqs89HZluLsh+XQSPHdiv//kJyhc5/isW7+z3t2jMfMJMBRLWaivZWSeDs8S3Fg6rIav4c7/r0jUKarMcNPhOLnR72uT9dykRrFEPWTuIDABuQft5oEck/MF2YymuRjLVsBeMuuOy4UyCnd8r06cPX4HoHMfpxRmzoJf68RSmBR+AmHN"
     ];
+
+    group = if isDarwin then "staff" else "crdant";
+
+    extraGroups = if isDarwin then
+      [ "admin" ]
+    else
+      [ "adm" "ssher" "sudo"];
   };
 
 
