2f2262643514c8f71c02216d048adaf921f0da0f
Chuck D'Antonio <chuck@replicated.com>
2024-04-18 12:10:02 -0400

    Installs sbctl with binary

--- pkgs/sbctl/default.nix
+++ pkgs/sbctl/default.nix
@@ -1,31 +1,28 @@
-{ stdenv, lib, buildGoModule, fetchFromGitHub, installShellFiles }:
+{ stdenv, lib, buildGoModule, fetchzip, installShellFiles }:
+let
+  os = if stdenv.isDarwin then "darwin" else "linux" ;
+  arch = if stdenv.isAarch64 then "arm64" else "amd64";
 
-buildGoModule rec {
+  isDarwin = stdenv.isDarwin;
+  isLinux = stdenv.isLinux;
+in stdenv.mkDerivation rec {
   pname = "troubeleshoot-sbctl";
-  version = "0.11.0";
+  version = "0.9.1";
 
-  src = fetchFromGitHub {
-    owner = "replicatedhq";
-    repo = "sbctl";
-    rev = "v${version}";
-    sha256 = "sha256-D1lNFVGQqMWn0OgD7WTnDg2E54JRI/ZjG1MSpmvQ4uA=";
+  src = fetchzip {
+    url = "https://github.com/replicatedhq/sbctl/releases/download/v${version}/sbctl_${os}_${arch}.tar.gz";
+    stripRoot = false;
+    sha256 = if isDarwin then
+        "sha256-EQV3972rcl7Yy87X/CFmiSSboxySFAVSbbQzRIGUaBs="
+      else
+        "";
   };
-
-  vendorHash = "sha256-kW21GdJ/eYee5/HUG6AlrkCpfzjvmQY8xFX71QQKpVk=";
-
-  # Override build phase to use make
-  buildPhase = ''
-    runHook preBuild
-    export GOPROXY=https://proxy.golang.org,direct
-    make build 
-    runHook postBuild
-  '';
 
   # Install the binary
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
-    cp bin/sbctl $out/bin/sbctl
+    install -m755 sbctl $out/bin/sbctl
     runHook postInstall
   '';
 
