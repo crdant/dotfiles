d57bd845221d81e9b8cd161a4892c6186c3ee7a9
Chuck D'Antonio <chuck@replicated.com>
2024-04-20 16:02:50 -0400

    Treats the old home as a new home

--- hosts/mash/hardware-configuration.nix
+++ hosts/mash/hardware-configuration.nix
@@ -21,7 +21,7 @@
       fsType = "vfat";
     };
 
-  fileSystems."/persist" =
+  fileSystems."/home" =
     { device = "/dev/disk/by-uuid/83e58c28-491e-47af-b5d7-eecb7e9dae95";
       fsType = "ext4";
     };
