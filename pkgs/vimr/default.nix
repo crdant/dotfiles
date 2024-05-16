0af86460b1024e75e82db54ac89e14c6207aaebe
Chuck D'Antonio <chuck@crdant.io>
2024-05-16 12:48:44 -0400

    Bumps VimR version

--- pkgs/vimr/default.nix
+++ pkgs/vimr/default.nix
@@ -3,11 +3,11 @@
 stdenv.mkDerivation rec {
   pname = "vimr";
   version = "v0.46.1";
-  build = "20240114.181346";
+  build = "20240426.143700"; 
 
   src = fetchurl {
     url = "https://github.com/qvacua/vimr/releases/download/${version}-${build}/VimR-v0.46.1.tar.bz2";
-    sha256 = "33aabbe736045f9901e89cef3fde7d8a758f39efca039ba7873c7961b79ee53a";
+    sha256 = "1g8l8j4grrnk8rvrp0a2d3724nz9hw6af09ij940frdr8bwvl2b9";
   };
 
   dontFixup = true;
