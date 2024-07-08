8b81f82d72ca2c8ff09f6c283f26cd775bee6c49
Chuck D'Antonio <chuck@replicated.com>
2024-07-08 13:06:45 -0400

    Attempts to get colors into iTerm2

--- users/crdant/defaults/iterm2.nix
+++ users/crdant/defaults/iterm2.nix
@@ -234,5 +234,423 @@
             "Red Component" = "0.916110634803772";
         };
     };
+    "Rosé Pine" = {
+      {
+        "Ansi 7 Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Link Color": {
+          "Red Component" = "0.76862746477127075";
+          "Blue Component" = "0.90588235855102539";
+          "Green Component" = "0.65490198135375977";
+        };
+        "Selected Text Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Cursor Text Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Ansi 15 Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Ansi 2 Color": {
+          "Red Component" = "0.61176472902297974";
+          "Blue Component" = "0.84705883264541626";
+          "Green Component" = "0.81176471710205078";
+        };
+        "Ansi 8 Color": {
+          "Red Component" = "0.43137255311012268";
+          "Blue Component" = "0.52549022436141968";
+          "Green Component" = "0.41568627953529358";
+        };
+        "Ansi 11 Color": {
+          "Red Component" = "0.96470588445663452";
+          "Blue Component" = "0.46666666865348816";
+          "Green Component" = "0.75686275959014893";
+        };
+        "Bold Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Ansi 3 Color": {
+          "Red Component" = "0.96470588445663452";
+          "Blue Component" = "0.46666666865348816";
+          "Green Component" = "0.75686275959014893";
+        };
+        "Ansi 9 Color": {
+          "Red Component" = "0.92156863212585449";
+          "Blue Component" = "0.57254904508590698";
+          "Green Component" = "0.43529412150382996";
+        };
+        "Ansi 14 Color": {
+          "Red Component" = "0.92156863212585449";
+          "Blue Component" = "0.729411780834198";
+          "Green Component" = "0.73725491762161255";
+        };
+        "Background Color": {
+          "Red Component" = "0.098039217293262482";
+          "Blue Component" = "0.14117647707462311";
+          "Green Component" = "0.090196080505847931";
+        };
+        "Ansi 10 Color": {
+          "Red Component" = "0.61176472902297974";
+          "Blue Component" = "0.84705883264541626";
+          "Green Component" = "0.81176471710205078";
+        };
+        "Ansi 4 Color": {
+          "Red Component" = "0.19215686619281769";
+          "Blue Component" = "0.56078433990478516";
+          "Green Component" = "0.45490196347236633";
+        };
+        "Cursor Color": {
+          "Red Component" = "0.3333333432674408";
+          "Blue Component" = "0.4117647111415863";
+          "Green Component" = "0.31764706969261169";
+        };
+        "Cursor Guide Color": {
+          "Red Component" = "0.70213186740875244";
+          "Blue Component": 1,
+          "Green Component" = "0.9268307089805603";
+        };
+        "Ansi 13 Color": {
+          "Red Component" = "0.76862746477127075";
+          "Blue Component" = "0.90588235855102539";
+          "Green Component" = "0.65490198135375977";
+        };
+        "Ansi 5 Color": {
+          "Red Component" = "0.76862746477127075";
+          "Blue Component" = "0.90588235855102539";
+          "Green Component" = "0.65490198135375977";
+        };
+        "Tab Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Badge Color": {
+          "Red Component" = "0.92156863212585449";
+          "Blue Component" = "0.729411780834198";
+          "Green Component" = "0.73725491762161255";
+        };
+        "Ansi 0 Color": {
+          "Red Component" = "0.14901961386203766";
+          "Blue Component" = "0.22745098173618317";
+          "Green Component" = "0.13725490868091583";
+        };
+        "Ansi 6 Color": {
+          "Red Component" = "0.92156863212585449";
+          "Blue Component" = "0.729411780834198";
+          "Green Component" = "0.73725491762161255";
+        };
+        "Selection Color": {
+          "Red Component" = "0.16470588743686676";
+          "Blue Component" = "0.21568627655506134";
+          "Green Component" = "0.15686275064945221";
+        };
+        "Ansi 12 Color": {
+          "Red Component" = "0.19215686619281769";
+          "Blue Component" = "0.56078433990478516";
+          "Green Component" = "0.45490196347236633";
+        };
+        "Foreground Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Ansi 1 Color": {
+          "Red Component" = "0.92156863212585449";
+          "Blue Component" = "0.57254904508590698";
+          "Green Component" = "0.43529412150382996";
+        };
+      };
+
+    };
+    "Rosé Pine Dawn" = {
+      {
+        "Ansi 7 Color": {
+          "Red Component" = "0.34117648005485535";
+          "Blue Component" = "0.47450980544090271";
+          "Green Component" = "0.32156863808631897";
+        };
+        "Link Color": {
+          "Red Component" = "0.56470590829849243";
+          "Blue Component" = "0.66274511814117432";
+          "Green Component" = "0.47843137383460999";
+        };
+        "Selected Text Color": {
+          "Red Component" = "0.34117648005485535";
+          "Blue Component" = "0.47450980544090271";
+          "Green Component" = "0.32156863808631897";
+        };
+        "Cursor Text Color": {
+          "Red Component" = "0.34117648005485535";
+          "Blue Component" = "0.47450980544090271";
+          "Green Component" = "0.32156863808631897";
+        };
+        "Ansi 15 Color": {
+          "Red Component" = "0.34117648005485535";
+          "Blue Component" = "0.47450980544090271";
+          "Green Component" = "0.32156863808631897";
+        };
+        "Ansi 2 Color": {
+          "Red Component" = "0.33725491166114807";
+          "Blue Component" = "0.62352943420410156";
+          "Green Component" = "0.58039218187332153";
+        };
+        "Ansi 8 Color": {
+          "Red Component" = "0.43137255311012268";
+          "Blue Component" = "0.52549022436141968";
+          "Green Component" = "0.41568627953529358";
+        };
+        "Ansi 11 Color": {
+          "Red Component" = "0.91764706373214722";
+          "Blue Component" = "0.20392157137393951";
+          "Green Component" = "0.61568629741668701";
+        };
+        "Bold Color": {
+          "Red Component" = "0.34117648005485535";
+          "Blue Component" = "0.47450980544090271";
+          "Green Component" = "0.32156863808631897";
+        };
+        "Ansi 3 Color": {
+          "Red Component" = "0.91764706373214722";
+          "Blue Component" = "0.20392157137393951";
+          "Green Component" = "0.61568629741668701";
+        };
+        "Ansi 9 Color": {
+          "Red Component" = "0.70588237047195435";
+          "Blue Component" = "0.47843137383460999";
+          "Green Component" = "0.38823530077934265";
+        };
+        "Ansi 14 Color": {
+          "Red Component" = "0.84313726425170898";
+          "Blue Component" = "0.49411764740943909";
+          "Green Component" = "0.50980395078659058";
+        };
+        "Background Color": {
+          "Red Component" = "0.98039215803146362";
+          "Blue Component" = "0.92941176891326904";
+          "Green Component" = "0.95686274766921997";
+        };
+        "Ansi 10 Color": {
+          "Red Component" = "0.33725491166114807";
+          "Blue Component" = "0.62352943420410156";
+          "Green Component" = "0.58039218187332153";
+        };
+        "Ansi 4 Color": {
+          "Red Component" = "0.15686275064945221";
+          "Blue Component" = "0.51372551918029785";
+          "Green Component" = "0.4117647111415863";
+        };
+        "Cursor Color": {
+          "Red Component" = "0.59607845544815063";
+          "Blue Component" = "0.64705884456634521";
+          "Green Component" = "0.57647061347961426";
+        };
+        "Cursor Guide Color": {
+          "Red Component" = "0.70213186740875244";
+          "Blue Component": 1,
+          "Green Component" = "0.9268307089805603";
+        };
+        "Ansi 13 Color": {
+          "Red Component" = "0.56470590829849243";
+          "Blue Component" = "0.66274511814117432";
+          "Green Component" = "0.47843137383460999";
+        };
+        "Ansi 5 Color": {
+          "Red Component" = "0.56470590829849243";
+          "Blue Component" = "0.66274511814117432";
+          "Green Component" = "0.47843137383460999";
+        };
+        "Tab Color": {
+          "Red Component" = "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Badge Color": {
+          "Red Component" = "0.84313726425170898";
+          "Blue Component" = "0.49411764740943909";
+          "Green Component" = "0.50980395078659058";
+        };
+        "Ansi 0 Color": {
+          "Red Component" = "0.94901961088180542";
+          "Blue Component" = "0.87058824300765991";
+          "Green Component" = "0.91372549533843994";
+        };
+        "Ansi 6 Color": {
+          "Red Component" = "0.84313726425170898";
+          "Blue Component" = "0.49411764740943909";
+          "Green Component" = "0.50980395078659058";
+        };
+        "Selection Color": {
+          "Red Component" = "0.93333333730697632";
+          "Blue Component" = "0.90196079015731812";
+          "Green Component" = "0.91372549533843994";
+        };
+        "Ansi 12 Color": {
+          "Red Component" = "0.15686275064945221";
+          "Blue Component" = "0.51372551918029785";
+          "Green Component" = "0.4117647111415863";
+        };
+        "Foreground Color": {
+          "Red Component" = "0.34117648005485535";
+          "Blue Component" = "0.47450980544090271";
+          "Green Component" = "0.32156863808631897";
+        };
+        "Ansi 1 Color": {
+          "Red Component" = "0.70588237047195435";
+          "Blue Component" = "0.47843137383460999";
+          "Green Component" = "0.38823530077934265";
+        };
+      };
+    };
+    "Rosé Pine Moon" = {
+      {
+        "Ansi 7 Color" = {
+          "Red Component" =  "0.87843137979507446";
+          "Blue Component" = "0.95686274766921997";
+          "Green Component" = "0.87058824300765991";
+        };
+        "Link Color" = {
+          "Red Component": "0.76862746477127075";
+          "Blue Component": "0.90588235855102539";
+          "Green Component": "0.65490198135375977";
+        };
+        "Selected Text Color" = {
+          "Red Component"  = "0.87843137979507446";
+          "Blue Component"  = "0.95686274766921997";
+          "Green Component"  = "0.87058824300765991";
+        };
+        "Cursor Text Color" = {
+          "Red Component"  = "0.87843137979507446";
+          "Blue Component"  = "0.95686274766921997";
+          "Green Component"  = "0.87058824300765991";
+        };
+        "Ansi 15 Color" = {
+          "Red Component"  = "0.87843137979507446";
+          "Blue Component"  = "0.95686274766921997";
+          "Green Component"  = "0.87058824300765991";
+        };
+        "Ansi 2 Color" = {
+          "Red Component"  = "0.61176472902297974";
+          "Blue Component"  = "0.84705883264541626";
+          "Green Component"  = "0.81176471710205078";
+        };
+        "Ansi 8 Color" = {
+          "Red Component"  = "0.5058823823928833";
+          "Blue Component"  = "0.61176472902297974";
+          "Green Component"  = "0.48627451062202454";
+        };
+        "Ansi 11 Color" = {
+          "Red Component"  = "0.96470588445663452";
+          "Blue Component"  = "0.46666666865348816";
+          "Green Component"  = "0.75686275959014893";
+        };
+        "Bold Color" = {
+          "Red Component"  = "0.87843137979507446";
+          "Blue Component"  = "0.95686274766921997";
+          "Green Component"  = "0.87058824300765991";
+        };
+        "Ansi 3 Color" = {
+          "Red Component"  = "0.96470588445663452";
+          "Blue Component"  = "0.46666666865348816";
+          "Green Component"  = "0.75686275959014893";
+        };
+        "Ansi 9 Color" = {
+          "Red Component"  = "0.92156863212585449";
+          "Blue Component"  = "0.57254904508590698";
+          "Green Component"  = "0.43529412150382996";
+        };
+        "Ansi 14 Color" = {
+          "Red Component"  = "0.91764706373214722";
+          "Blue Component"  = "0.59215688705444336";
+          "Green Component"  = "0.60392159223556519";
+        };
+        "Background Color" = {
+          "Red Component"  = "0.13725490868091583";
+          "Blue Component"  = "0.21176470816135406";
+          "Green Component"  = "0.12941177189350128";
+        };
+        "Ansi 10 Color" = {
+          "Red Component"  = "0.61176472902297974";
+          "Blue Component"  = "0.84705883264541626";
+          "Green Component"  = "0.81176471710205078";
+        };
+        "Ansi 4 Color" = {
+          "Red Component"  = "0.24313725531101227";
+          "Blue Component"  = "0.69019609689712524";
+          "Green Component"  = "0.56078433990478516";
+        };
+        "Cursor Color" = {
+          "Red Component"  = "0.3490196168422699";
+          "Blue Component"  = "0.42745098471641541";
+          "Green Component"  = "0.32941177487373352";
+        };
+        "Cursor Guide Color" = {
+          "Red Component"  = "0.70213186740875244";
+          "Blue Component"  = "1";
+          "Green Component"  = "0.9268307089805603";
+        };
+        "Ansi 13 Color" = {
+          "Red Component"  = "0.76862746477127075";
+          "Blue Component"  = "0.90588235855102539";
+          "Green Component"  = "0.65490198135375977";
+        };
+        "Ansi 5 Color" = {
+          "Red Component"  = "0.76862746477127075";
+          "Blue Component"  = "0.90588235855102539";
+          "Green Component"  = "0.65490198135375977";
+        };
+        "Tab Color" = {
+          "Red Component"  = "0.87843137979507446";
+          "Blue Component"  = "0.95686274766921997";
+          "Green Component"  = "0.87058824300765991";
+        };
+        "Badge Color" = {
+          "Red Component"  = "0.91764706373214722";
+          "Blue Component"  = "0.59215688705444336";
+          "Green Component"  = "0.60392159223556519";
+        };
+        "Ansi 0 Color" = {
+          "Red Component"  = "0.22352941334247589";
+          "Blue Component"  = "0.32156863808631897";
+          "Green Component"  = "0.20784313976764679";
+        };
+        "Ansi 6 Color" = {
+          "Red Component"  = "0.91764706373214722";
+          "Blue Component"  = "0.59215688705444336";
+          "Green Component"  = "0.60392159223556519";
+        };
+        "Selection Color" = {
+          "Red Component"  = "0.19215686619281769";
+          "Blue Component"  = "0.26666668057441711";
+          "Green Component"  = "0.18431372940540314";
+        };
+        "Ansi 12 Color" = {
+          "Red Component"  = "0.24313725531101227";
+          "Blue Component"  = "0.69019609689712524";
+          "Green Component"  = "0.56078433990478516";
+        };
+        "Foreground Color" = {
+          "Red Component"  = "0.87843137979507446";
+          "Blue Component"  = "0.95686274766921997";
+          "Green Component"  = "0.87058824300765991";
+        };
+        "Ansi 1 Color" = {
+          "Red Component"  = "0.92156863212585449";
+          "Blue Component"  = "0.57254904508590698";
+          "Green Component"  = "0.43529412150382996";
+        };
+      };
+    };
   };
 }
