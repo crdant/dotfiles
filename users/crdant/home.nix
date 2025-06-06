bb68771dc50544fc85985f533a5366fbc9df1f9a
Chuck D'Antonio <chuck@crdant.io>
2025-06-06 18:28:06 -0400

    Removes second pass of arguments

--- users/crdant/home.nix
+++ users/crdant/home.nix
@@ -1,13 +1,8 @@
-{ inputs, outputs, config, pkgs, lib, username, homeDirectory, gitEmail, kind, ... }:
+{ inputs, outputs, config, pkgs, lib, username, homeDirectory, gitEmail, ... }:
 
 {
   # Import the full profile by default
   imports = [
     ./profiles/full.nix
   ];
-  
-  # Pass necessary parameters to the imported modules
-  _module.args = {
-    inherit inputs outputs username homeDirectory gitEmail kind;
-  };
 }
