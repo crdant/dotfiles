2098570c458f6f2e3a36275a238c760a7f2f14eb
Chuck D'Antonio <chuck@replicated.com>
2025-06-25 21:17:48 -0400

    Adds some server configurations to run hardened stuff

--- home/profiles/server.nix
+++ home/profiles/server.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
-  # AI-focused profile for working with AI tools and assistants
+  # AI-focused profile for a home environment on a remote server
   imports = [
     ../modules/base
     ../modules/security
+    ../modules/editor
+    ../modules/homelab
   ] ;
 }
