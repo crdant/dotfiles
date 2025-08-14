bc77e7dff0a7157b2064adbcd3a05131c6064900
Chuck D'Antonio <chuck@replicated.com>
2025-08-14 15:57:28 -0700

    Provides a static IP for Swan

--- systems/hosts/swan/default.nix
+++ systems/hosts/swan/default.nix
@@ -13,5 +13,24 @@
       enable = true;
       allowedTCPPorts = [ 22 ];
     };
+    interfaces = {
+      eth0 = {
+        ipv4 = {
+          addresses = [
+            {
+              address = "10.105.0.253";
+              prefixLength = 24;
+            }
+          ];
+        };
+      };
+    };
+
+    defaultGateway = "10.105.0.1";
+    nameservers = [
+      "10.105.0.1"
+      # "10.105.0.253"
+      # "10.105.0.254"
+    ];
   };
 } 
