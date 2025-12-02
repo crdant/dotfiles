6b5bc999b1fda1b31f728ce8cc518cfb09766363
Chuck D'Antonio <chuck@crdant.io>
2025-12-01 20:45:20 -0500

    Adds support for IPMI over SSH
    TL;DR
    -----
    
    Configures SSH to access IPMI
    
    Details
    -------
    
    Updates the `homelab` home manager module to simplify connecting to my
    three Supermicro servers via IPMI over SSH.

--- home/modules/homelab/default.nix
+++ home/modules/homelab/default.nix
@@ -19,6 +19,12 @@ in {
         source = ./config/ssh/config.d;
         recursive = true;
       };
+      ".ssh/id_homelab.pub" ={
+        text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDt39SrVyQAVPHEEkfjekQYO1GSrUU9UvBrznXgOcz60";
+      };
+      ".ssh/id_ipmi.pub" ={
+        text = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCgNg8ItxlZaCyyQ29V1Lp/0VHF6I7BWWBoU93djho2MGHK3neQEUnqV4/W9IbRVWOwKecORok3LZRunQ71MLNTynEnamHTtZ+OXbX3BBcaKR1vbPRsegVNmrHDANNBG6lh+J27C3LMvfE+pzdFxnPRzMCP2uA2sA/2nnFAC4p1u5CXU9ThqiaFPMi+Kjii+6CgQCaB1ExQQMJ/SCi/PQ16i8jZ+ibMxp5a/ms1hn/9W0QgGHAGSASspTINcP8mFwPjKFYnp2UJ2AKAMky9ifFt5U1sHEqZgRlwd2TdT3SpRxwPevHdNMxrh4DhJ7+dNuiPRDnyEqFBOcyuu1W8njZw4F/8q0pyTq2s7qPygDSDNJwbzz1gJEnJL3y31SiZDK+ZgHIFUCpTBqelyksZrXb4DX5tb1QN9vYbYG9Yc4J5Yp3qwPQ/6NQvbQV3V9b1In1rz6w+wgVxJ7OC7eDcA39vaKBnXJv37UbpaNtY6bOdHvzDApIsJg2YR34+IgJEble1AXPA4M1yuUswThNoztcZjbgfrEf/pRpgtkr74k2ylmuM4+C1AbWNfnGxxW6C1WZfBYZn0pD+uxbXqXRP+Qm8/WNueuo2K/TnHvhJkrdUXv2m4MdH87jwPjZwf9YPRhHxEYg0N5PoeixajJ9NGPhEblABaozVk0IqlbRcsFPAzw==";
+      };
     } // lib.optionalAttrs pkgs.stdenv.isDarwin {
       "Library/Application Support/espanso/match/snippets.yml" = {
         source = ./config/espanso/match/snippets.yml;
@@ -36,6 +42,16 @@ in {
           user = "root";
           identityFile = "~/.ssh/id_homelab.pub";
           extraOptions = {
+            identityAgent = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+            identitiesOnly = "yes";
+          };
+        };
+        "ipmi.rye.lab.shortrib.net ipmi.bourbon.lab.shortrib.net ipmi.scotch.lab.shortrib.net" = {
+          user = "arceus";
+          identityFile = "~/.ssh/id_ipmi.pub";
+          extraOptions = {
+            hostKeyAlgorithms = "+ssh-rsa";
+            passwordAuthentication = "yes";
             identityAgent = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
             identitiesOnly = "yes";
           };
