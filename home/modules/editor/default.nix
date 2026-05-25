8e76f7093bfe17b9701ce5f763a86348259a8ed4
Chuck D'Antonio <chuck@crdant.io>
2026-05-24 21:21:23 -0400

    Disable supermaven-nvim initialization

--- home/modules/editor/default.nix
+++ home/modules/editor/default.nix
@@ -110,8 +110,8 @@ in {
 
         vim.api.nvim_set_keymap("n", "<C-P>", ":Files<CR>", { noremap = true })
         
-        -- Supermaven setup
-        require('supermaven-nvim').setup({})
+        -- Supermaven setup (disabled)
+        -- require('supermaven-nvim').setup({})
         
         -- Challenge Edit functionality
         function ChallengeEdit(args)
