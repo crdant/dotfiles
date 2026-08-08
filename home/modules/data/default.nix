307c13f3a7b686b26663597a5ba64dd792888c18
Chuck D'Antonio <chuck@crdant.io>
2026-08-08 08:55:50 -0400

    Remove Snowflake tooling from the data module
    Drop snowsql, its private-key secret, config, and shell alias now that
    Replicated's Snowflake account is gone.

--- home/modules/data/default.nix
+++ home/modules/data/default.nix
@@ -8,21 +8,8 @@ in {
   home.packages = with pkgs; [
     python313Packages.jupytext
   ] ++ lib.optionals isLinux [
-    snowsql
   ];
-
-  sops.secrets."snowflake/private_key" = {
-    path = "${config.home.homeDirectory}/.ssh/id_snowflake.p8";
-    mode = "0600";
-  };
-
-  home.file = {
-    ".snowsql" = {
-      source = ./config/snowsql;
-      recursive = true;
-    };
-  };
-  
+ 
   programs = {
     neovim = {
       plugins = with pkgs.vimPlugins; [
@@ -31,10 +18,5 @@ in {
       ];
     };
 
-    zsh = {
-      shellAliases = lib.mkIf isDarwin {
-        snowsql = "/Applications/SnowSQL.app/Contents/MacOS/snowsql";
-      };
-    };
   };
 }
