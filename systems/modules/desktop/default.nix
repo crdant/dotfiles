c22067dfba4eaff38fd22f4055de274dbfd819e7
Chuck D'Antonio <crdant@shortrib.io>
2026-09-22 12:48:46 -0400

    Add the JetBrains Mono nerd font

--- systems/modules/desktop/default.nix
+++ systems/modules/desktop/default.nix
@@ -107,6 +107,7 @@ in (lib.mkMerge [
       packages = with pkgs.nerd-fonts; [
         fira-code
         inconsolata
+        jetbrains-mono
         noto
         bitstream-vera-sans-mono
       ];
